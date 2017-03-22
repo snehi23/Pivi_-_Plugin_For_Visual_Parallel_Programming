c21a37b22920e12f14c3fd75520dd8c48254ca68
srikiranpanchavati <srikiran.phoenix@gmail.com>
2017-03-22 11:25:23 -0700

    added package for generated code

---
+++ PiviParser/src/asu/ser/capstone/pivi/test/Solution.java
@@ -0,0 +1,13 @@
+package asu.ser.capstone.pivi.test;
+
+public class Solution {
+	public static void main(String[] args) {
+		System.out.println("hello");
+		int a = 10;
+		if (a % 2 == 0) {
+			System.out.println("even number");
+		} else {
+			System.out.println("odd number");
+		}
+	}
+}
