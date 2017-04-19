baf8198c9e47fb0a47852c9eadc1a364c222c477
pujitha9 <pkara1@asu.edu>
2017-04-19 11:46:18 -0700

    If statement popup implementation

---
+++ PiviPlugin.diagram/src/asu/ser/capstone/pivi/diagram/edit/policies/custom/IfStatementOpenEditPolicy.java
@@ -0,0 +1,53 @@
+package asu.ser.capstone.pivi.diagram.edit.policies.custom;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.Request;
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.OpenEditPolicy;
+import org.eclipse.jface.window.Window;
+import org.eclipse.jface.wizard.IWizard;
+import org.eclipse.jface.wizard.WizardDialog;
+
+import asu.ser.capstone.pivi.diagram.edit.commands.custom.EditInstructionCommandThread;
+import asu.ser.capstone.pivi.diagram.edit.parts.IfStartIfStartFigureCompartmentEditPart;
+import asu.ser.capstone.pivi.diagram.edit.parts.InstructionInstructionFigureCompartmentEditPart;
+import asu.ser.capstone.pivi.diagram.part.custom.InstructionWizard;
+import asu.ser.capstone.pivi.diagram.part.custom.IfStatementWizard;;
+
+public class IfStatementOpenEditPolicy extends OpenEditPolicy{
+
+	private WizardDialog wizardDialog;
+
+	public IfStatementOpenEditPolicy(WizardDialog wizardDialog) {
+		super();
+		this.wizardDialog = wizardDialog;
+	}
+
+	@Override
+	protected Command getOpenCommand(Request request) {
+
+		if (wizardDialog.open() == Window.OK) {
+			IWizard wizard = wizardDialog.getCurrentPage().getWizard();
+			if (wizard instanceof IfStatementWizard) {
+				EditPart editPart = getTargetEditPart(request);
+				if (editPart != null && editPart instanceof IfStartIfStartFigureCompartmentEditPart) {
+					EditPart ifStatementEditPart = editPart.getParent();
+					if (ifStatementEditPart != null) {
+						GraphicalEditPart ifStatementGEP = ((GraphicalEditPart) editPart);
+						TransactionalEditingDomain domain = ifStatementGEP.getEditingDomain();
+						EObject modelElement = ifStatementGEP.resolveSemanticElement();
+						EStructuralFeature feature = modelElement.eClass().getEStructuralFeature("instructions");
+						Thread editInstruction = new Thread(new EditInstructionCommandThread(domain, feature, modelElement, wizard));
+						editInstruction.start();
+					}
+				}
+			}
+		}
+		return null;
+	}
+
+}
--- PiviPlugin.diagram/src/asu/ser/capstone/pivi/diagram/part/custom/IfStatementWizard.java
+++ PiviPlugin.diagram/src/asu/ser/capstone/pivi/diagram/part/custom/IfStatementWizard.java
@@ -1,5 +1,76 @@
 package asu.ser.capstone.pivi.diagram.part.custom;
 
-public class IfStatementWizard {
+import org.eclipse.gmf.runtime.diagram.ui.editparts.GraphicalEditPart;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
 
+import asu.ser.capstone.pivi.Instruction;
+
+public class IfStatementWizard extends Wizard{
+
+	private class IfStatementPage extends WizardPage {
+
+		public Text conditionText;
+
+		public IfStatementPage(String pageName) {
+			super(pageName);
+			setTitle("Condition");
+		}
+
+		@Override
+		public void createControl(Composite parent) {
+			Composite composite = new Composite(parent, SWT.NONE);
+			Label conditionLabel = new Label(composite, SWT.NONE);
+			conditionLabel.setText("If Condition:");
+			GridData gridData = new GridData();
+			gridData.horizontalSpan = 2;
+			conditionLabel.setLayoutData(gridData);
+			conditionText = new Text(composite, SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
+			gridData = new GridData();
+			gridData.horizontalAlignment = SWT.FILL;
+			gridData.grabExcessHorizontalSpace = true;
+			gridData.verticalAlignment = SWT.FILL;
+			gridData.grabExcessVerticalSpace = true;
+			conditionText.setLayoutData(gridData);
+			conditionText.setText(newCondition);
+			composite.setLayout(new GridLayout(2, false));
+			setControl(composite);
+		}
+	}
+
+	private String newCondition;
+
+	public IfStatementWizard(GraphicalEditPart ifStartEditPart) {
+		if (ifStartEditPart != null) {
+			Instruction ifStatementModel = (Instruction) ifStartEditPart.resolveSemanticElement();
+			if (ifStatementModel != null && ifStatementModel.getInstructions() != null) {
+				this.newCondition = ifStatementModel.getInstructions();
+			} else {
+				this.newCondition = "";
+			}
+			
+		} else {
+			this.newCondition = "";
+		}
+		addPage(new IfStatementPage("ifStatementPage"));
+	}
+
+	@Override
+	public boolean performFinish() {
+		IfStatementPage page = (IfStatementPage) getPage("ifStatementPage");
+		newCondition = page.conditionText.getText();
+		return true;
+	}
+
+	public String getNewCondition() {
+		return newCondition;
+	}
+
+	
 }
